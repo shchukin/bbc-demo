6eab06180356bf1a84b1acbb18f5758ac556362c
Anton Shchukin <a.a.shchukin@gmail.com>
2025-09-11 18:42:30 +0700

    carousel 2

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -42,6 +42,44 @@
             });
         }
 
+        if( $carousel.classList.contains('carousel--js-init-works') ) {
+            new Swiper($carousel.querySelector('.swiper'), {
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+                spaceBetween: 10,
+                autoHeight: true,
+                speed: 600,
+                pagination: {
+                    el: $carousel.querySelector('.carousel__pagination'),
+                    type: "fraction", /* можно переделать на bullets или сделать всю секцию false */
+                    bulletClass: 'carousel__bullet',
+                    bulletActiveClass: 'carousel__bullet--current',
+                    clickable: true
+                },
+                navigation: {
+                    prevEl: $carousel.querySelector('.carousel__button--prev'),
+                    nextEl: $carousel.querySelector('.carousel__button--next'),
+                    disabledClass: 'carousel__button--disabled',
+                },
+                // breakpoints: {
+                //     400: {
+                //         slidesPerView: 2,
+                //         slidesPerGroup: 2,
+                //     },
+                //     740: {
+                //         slidesPerView: 3,
+                //         slidesPerGroup: 3,
+                //         spaceBetween: 54,
+                //     },
+                //     1850: {
+                //         slidesPerView: 5,
+                //         slidesPerGroup: 5,
+                //         spaceBetween: 84,
+                //     }
+                // },
+            });
+        }
+
     });
 
 
