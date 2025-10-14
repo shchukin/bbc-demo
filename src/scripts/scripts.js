0491f7c4fb842b607a3dbc40cac473501f21779d
Anton Shchukin <a.a.shchukin@gmail.com>
2025-10-14 20:55:32 +0700

    bubbles

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,26 @@
 (function($) {
 
+    /* Баблы */
+
+    /* Показ (тап на смартфонах) */
+    $('.bubble-handler').on('click', function () {
+        var $target = $(this).parents('.bubble-context').find('.bubble');
+        $('.bubble--visible').not($target).removeClass('bubble--visible');
+        $target.toggleClass('bubble--visible');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.bubble, .bubble-handler').length) {
+            $('.bubble--visible').removeClass('bubble--visible');
+        }
+    });
+
+    $(document).on('keyup', function(event) {
+        if (event.keyCode === 27) {
+            $('.bubble--visible').removeClass('bubble--visible');
+        }
+    });
+
 
     /* Init magnific popup */
 
